aa819136c3c2db89178dd1ad617f9ecf6705363b
brianngeno <bkn.ngeno@gmail.com>
2018-07-11 22:59:55 +0300

    added a menu bar and styled it to color white and also gave it a margin-top

--- css/scripts.js
+++ css/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $("#subscribe").click(function() {
+  $("#subscribe").click(function(event) {
     alert("You have succesfully subscribed");
+    var email = $("input#email").val();
+    event.preventDefault();
+    console.log(email);
   });
 });
